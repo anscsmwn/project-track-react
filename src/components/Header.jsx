ab554399be163e1873ae0289a51a318391237255
Annas Casmawan Ahmad <annasnganro.dev@gmail.com>
2024-01-03 12:54:56 +0800

    feat: add logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import supabase from '../supabase/supabaseClient'
 
 const Header = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false)
-
+  const navigate = useNavigate()
   const toggleDropdown = () => {
     setIsDropdownVisible(!isDropdownVisible)
   }
+  const initialName =
+    JSON.parse(localStorage.getItem('profile')).first_name[0].toUpperCase() +
+    JSON.parse(localStorage.getItem('profile')).last_name[0].toUpperCase()
+
   return (
     <header className="px-5 sm:px-10 py-4 flex justify-between items-center">
       <p>
@@ -13,11 +19,27 @@ const Header = () => {
           Undergraduate Thesis Track{' '}
         </span>
       </p>
-      <div className="profile-container">
-        <div onClick={toggleDropdown} className="profile-icon">
-          {/* Replace "AA" with the actual profile image */}
-          <span className="profile-initials">AA</span>
+      <div className="profile-container relative">
+        <div onClick={toggleDropdown} className="profile-icon cursor-pointer">
+          <span className="profile-initials">{initialName}</span>
         </div>
+        {isDropdownVisible && (
+          <div className="dropdown-menu absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+            <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              Profile
+            </Link>
+            <button
+              onClick={async () => {
+                await supabase.auth.signOut()
+                localStorage.clear()
+                navigate('/login')
+              }}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </div>
     </header>
   )
