84359a880a8de74c7bc090fffece6a5843f38981
Annas Casmawan Ahmad <annasnganro.dev@gmail.com>
2024-01-17 10:37:35 +0800

    feat: user can edit profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { getStorangeProfile, getUserId } from '../utils/utils'
-import { getProfile, updateProfile } from '../services/Profile'
+import {
+  getProfile,
+  getProjectInformation,
+  updateProfile,
+  updateProjectInformation,
+} from '../services/Profile'
 
 const Profile = () => {
   const [profile, setProfile] = React.useState({})
+  const [project, setProject] = React.useState({})
   const [loading, setLoading] = React.useState(true)
+  const [activeTab, setActiveTab] = React.useState('profile')
   const profileId = getUserId()
   React.useEffect(() => {
     setLoading(true)
     const profile = getStorangeProfile()
     setProfile(profile)
-    const fetchProfile = async () => {
-      const response = await getProfile(profileId)
-      setProfile(response[0])
+    const fetchData = async () => {
+      const responseProfile = await getProfile(profileId)
+      const responseProject = await getProjectInformation(profileId)
+      setProfile(responseProfile[0])
+      setProject(responseProject[0])
     }
-    fetchProfile()
+    fetchData()
     setLoading(false)
   }, [])
 
-  const onSubmit = async (e) => {
+  const onSubmitProfile = async (e) => {
     e.preventDefault()
     setLoading(true)
     await updateProfile({
@@ -29,62 +38,139 @@ const Profile = () => {
     })
     setLoading(false)
   }
+  const onSubmitProject = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    await updateProjectInformation({
+      id: profileId,
+      title: project.title,
+      description: project.description,
+    })
+    setLoading(false)
+  }
   return (
     <Layout>
       <main className="flex justify-center mt-10">
-        <div className="max-w-md">
-          <h1 className="text-xl font-semibold mb-2">
-            Manage your personal information
-          </h1>
+        <div className="max-w-lg">
+          <h1 className="text-xl font-semibold mb-2">Kelola Informasi Anda</h1>
           <p className="text-gray-600 text-sm mb-4">
-            This is an account. Edit your personal information and others
-            settings through this page.
+            Kelola informasi pribadi Anda dan pengaturan lain melalui halaman
+            ini.
           </p>
-          <form onSubmit={onSubmit}>
-            <div className="flex flex-col space-y-2 my-3">
-              <label className="label" htmlFor="firstname">
-                First Name
-              </label>
-              <input
-                name="firstname"
-                className="input-field"
-                type="text"
-                id="firstname"
-                defaultValue={profile.first_name}
-                onChange={(e) => {
-                  setProfile({
-                    ...profile,
-                    first_name: e.target.value,
-                  })
-                }}
-                required
-              />
-              <label className="label" htmlFor="lastname">
-                Last Name
-              </label>
-              <input
-                name="lastname"
-                className="input-field"
-                type="lastname"
-                id="lastname"
-                defaultValue={profile.last_name}
-                onChange={(e) => {
-                  setProfile({
-                    ...profile,
-                    last_name: e.target.value,
-                  })
-                }}
-                required
-              />
-            </div>
+          <div className="text-xs flex gap-2 items-center">
             <button
-              disabled={loading}
-              className="px-3 py-2 text-sm w-full rounded-md text-white font-semibold bg-black border border-solid border-black disabled:opacity-50"
-              type="submit"
+              onClick={() => setActiveTab('profile')}
+              className={
+                activeTab === 'profile'
+                  ? 'bg-black px-3 py-2 rounded-full text-white'
+                  : 'px-3 py-2'
+              }
             >
-              {loading ? 'Loading...' : 'Save'}
+              Personal
             </button>
-          </form>
+            <button
+              onClick={() => setActiveTab('project')}
+              className={
+                activeTab === 'project'
+                  ? 'bg-black px-3 py-2 rounded-full text-white'
+                  : 'px-3 py-2'
+              }
+            >
+              Project
+            </button>
+          </div>
+          {activeTab === 'profile' && (
+            <form onSubmit={onSubmitProfile}>
+              <div className="flex flex-col space-y-2 my-3">
+                <label className="label" htmlFor="firstname">
+                  First Name
+                </label>
+                <input
+                  name="firstname"
+                  className="input-field"
+                  type="text"
+                  id="firstname"
+                  defaultValue={profile.first_name}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      first_name: e.target.value,
+                    })
+                  }}
+                  required
+                />
+                <label className="label" htmlFor="lastname">
+                  Last Name
+                </label>
+                <input
+                  name="lastname"
+                  className="input-field"
+                  type="lastname"
+                  id="lastname"
+                  defaultValue={profile.last_name}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      last_name: e.target.value,
+                    })
+                  }}
+                  required
+                />
+              </div>
+              <button
+                disabled={loading}
+                className="px-3 py-2 text-sm w-full rounded-md text-white font-semibold bg-black border border-solid border-black disabled:opacity-50"
+                type="submit"
+              >
+                {loading ? 'Loading...' : 'Save'}
+              </button>
+            </form>
+          )}
+          {activeTab === 'project' && (
+            <form onSubmit={onSubmitProject}>
+              <div class="flex flex-col space-y-2 my-3">
+                <label class="label" for="title">
+                  Judul Tugas Akhir
+                </label>
+                <textarea
+                  defaultValue={project.title}
+                  class="input-field"
+                  id="title"
+                  rows="3"
+                  onChange={(e) => {
+                    setProject({
+                      ...project,
+                      title: e.target.value,
+                    })
+                  }}
+                  required
+                />
+                <label class="label" for="description">
+                  Deskripsi Tugas Akhir
+                </label>
+                <textarea
+                  defaultValue={project.description}
+                  class="input-field"
+                  id="description"
+                  rows="8"
+                  onChange={(e) => {
+                    setProject({
+                      ...project,
+                      description: e.target.value,
+                    })
+                  }}
+                  required
+                />
+              </div>
+              <button
+                disabled={loading}
+                class="px-3 py-2 text-sm w-full rounded-md text-white font-semibold bg-black border border-solid border-black disabled:opacity-50"
+                type="submit"
+              >
+                {loading ? 'Loading...' : 'Simpan'}
+              </button>
+            </form>
+          )}
         </div>
       </main>
     </Layout>
--- src/services/Profile.js
+++ src/services/Profile.js
@@ -17,11 +17,39 @@ export const getProfile = async (profileId) => {
   return data
 }
 
+export const getProjectInformation = async (profileId) => {
+  const { data, error } = await supabase
+    .from('students')
+    .select(
+      `
+      id,
+      title,
+      description
+    `,
+    )
+    .eq('id', profileId)
+  if (error) {
+    throw new Error(error.message)
+  }
+  return data
+}
+
 export const updateProfile = async (profile) => {
   const { data, error } = await supabase
     .from('users')
     .update(profile)
     .eq('id', profile.id)
+  if (error) {
+    throw new Error(error.message)
+  }
+  return data
+}
+
+export const updateProjectInformation = async (project) => {
+  const { data, error } = await supabase
+    .from('students')
+    .update(project)
+    .eq('id', project.id)
   if (error) {
     throw new Error(error.message)
   }
